717a85e41c27b35ba83d655cc4aa59759a20766d
ayd <anthonypdawson@gmail.com>
2013-01-26 17:25:49 -0500

    Added movement keys for a gamepad

--- Spaceship/Spaceship/IO/PlayerInput.cs
+++ Spaceship/Spaceship/IO/PlayerInput.cs
@@ -8,16 +8,56 @@ namespace Spaceship.IO
 {
     public static class PlayerInput
     {
-        public static readonly Keys[] MovementKeys = new Keys[] { Keys.Left, Keys.Right, Keys.Up, Keys.Down };
+        public static readonly Keys[] MovementKeys =
+            new Keys[] 
+            { 
+                Keys.Left, 
+                Keys.Right, 
+                Keys.Up, 
+                Keys.Down 
+            };
+
+        public static readonly Buttons[] MovementButtons =
+            new Buttons[] 
+        { 
+            Buttons.LeftThumbstickLeft, 
+            Buttons.LeftThumbstickRight, 
+            Buttons.LeftThumbstickUp, 
+            Buttons.LeftThumbstickDown 
+        };
+
+        public static Boolean HasMovement(Keys[] keys = null, Buttons[] buttons = null)
+        {
+            bool isMovement = false;
+            if (keys != null)
+                isMovement = HasMovement(keys);
+            if (buttons != null && !isMovement)
+                isMovement = HasMovement(buttons);
+
+            return isMovement;
+
+
+        }
 
         public static Boolean IsMovement(Keys key)
         {
             return MovementKeys.Contains(key);
         }
 
+        public static Boolean IsMovement(Buttons button)
+        {
+            return MovementButtons.Contains(button);
+        }
+
         public static Boolean HasMovement(Keys[] keys)
         {
             return keys.Any(k => IsMovement(k));
         }
+
+        public static Boolean HasMovement(Buttons[] buttons)
+        {
+            return buttons.Any(b => IsMovement(b));
+        }
+
     }
 }
